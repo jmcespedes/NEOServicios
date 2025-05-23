03a5a42b674313c3836db47f52f47e54f4013d92
jmcespedes <cespedes.josem@gmail.com>
2025-05-22 20:54:56 -0400

    Actualiza app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ from db import (
 app = Flask(__name__)
 CORS(app, origins=["https://www.neoservicios.cl"])
 
-@app.before_request
-def handle_options():
-    if request.method == 'OPTIONS':
-        response = make_response()
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        return response
+## @app.before_request
+##def handle_options():
+##    if request.method == 'OPTIONS':
+##        response = make_response()
+##        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+##        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+##        return response
 
 iconos_servicios = {
     "Flete": "🚚",
